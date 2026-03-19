d5661e8ef435a7b905e50a768930de324b8841d7
AlexeiGitHub <0xaker0@gmail.com>
2026-03-19 15:43:26 +0200

    FRAM-99-fix-sidebar return routes.ts

---
+++ shared/comfig/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  USERS: 'users',
+  PAYMENTS: 'payments',
+  POSTS: 'posts',
+  STATISTICS: 'statistics',
+  EMPTY: '',
+} as const;
+
+export type routes = (typeof ROUTES)[keyof typeof ROUTES];
